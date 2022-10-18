5ad2b1939a041ff5e46fd5ce508a02ba10ed28db
Tom Beach <beachth@cf.ac.uk>
2022-10-18 20:20:51 +0100

    Fix Build

--- src/components/IFCViewer.vue
+++ src/components/IFCViewer.vue
@@ -16,6 +16,7 @@ import { IFCDOOR, IFCSPACE, IFCOPENINGELEMENT, IFCFURNISHINGELEMENT, IFCWALL, IF
   
   onMounted(async() => {
     const viewer = new IfcViewerAPI({ container: container.value });
+    viewer.IFC.setWasmPath(window.location.origin);
     await viewer.IFC.loader.ifcManager.useWebWorkers(true, "../IFCWorker.js");
     await viewer.IFC.loader.ifcManager.applyWebIfcConfig({COORDINATE_TO_ORIGIN: true,USE_FAST_BOOLS: true});
     await viewer.IFC.loader.ifcManager.parser.setupOptionalCategories({[IFCDOOR]:true,[IFCSPACE]: false,[IFCOPENINGELEMENT]: false});
